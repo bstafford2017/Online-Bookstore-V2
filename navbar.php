2dde5a706acd1b7d73d97fdade473766b79a1783
b.stafford2017 <b.stafford2017@gmail.com>
2019-11-28 17:27:30 -0600

    test

--- navbar.php
+++ navbar.php
@@ -4,8 +4,8 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav col-sm-12">
+    <div class="collapse navbar-collapse col-sm-12" id="navbarNav">
+        <ul class="navbar-nav">
             <li class="nav-item">
                 <a id="home" class="nav-link" href="index.php">Home</a>
             </li>
