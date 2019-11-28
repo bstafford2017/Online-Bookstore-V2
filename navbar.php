625c785ab83974417608ca49cb00eaa2ce28b367
b.stafford2017 <b.stafford2017@gmail.com>
2019-11-28 13:38:00 -0600

    add sign out

--- navbar.php
+++ navbar.php
@@ -1,6 +1,5 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="margin-left: 10%;margin-right: 10%;">
-    <img src="favicon.png" width="30" height="30" class="d-inline-block align-top" alt="">
     <a class="navbar-brand" href="index.php">My Bookstore</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -26,8 +25,8 @@
     </div>
     <div class="collapse navbar-collapse">
         <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-                <a class="nav-link" href="signout.php">Sign Out</a>
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">Sign Out</a>
             </li>
         </ul>
     </div>
