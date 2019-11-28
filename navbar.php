99e20255c499f6cdd06e53c480fe3d887976d7e4
b.stafford2017 <b.stafford2017@gmail.com>
2019-11-28 13:33:56 -0600

    test

--- navbar.php
+++ navbar.php
@@ -1,5 +1,6 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="margin-left: 10%;margin-right: 10%;">
+    <img src="favicon.png" width="30" height="30" class="d-inline-block align-top" alt="">
     <a class="navbar-brand" href="index.php">My Bookstore</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -20,6 +21,13 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="listcustomers.php">List Customers</a>
+            </li>
+        </ul>
+    </div>
+    <div class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+                <a class="nav-link" href="signout.php">Sign Out</a>
             </li>
         </ul>
     </div>
