7756cf35729b19bfb7e637ba108b34f8a3a73717
b.stafford2017 <b.stafford2017@gmail.com>
2019-11-27 19:08:10 -0600

    update search for many searches

--- cgi-bin/Search.java
+++ cgi-bin/Search.java
@@ -8,22 +8,6 @@ import java.util.Iterator;
 import java.util.Comparator;
 import java.util.Collections;
 
-class Tuple {
-    int count;
-    long isbn;
-    String title;
-    double price;
-    LinkedList<String> subjects = new LinkedList<>();
-
-    public Tuple(int count, long isbn, String title, double price, String subject){
-        this.count = count;
-        this.isbn = isbn;
-        this.title = title;
-        this.price = price;
-        this.subjects.add(subject);
-    }
-}
-
 public class Search {
     public static void main(String[] args) throws SQLException {
         String user     = "C##benjamin.stafford";
@@ -37,9 +21,16 @@ public class Search {
         Statement stmt = conn.createStatement();
         try {
             String query = "SELECT COUNT(*), isbn, title, price, COLUMN_VALUE FROM book b, table(b.subjects) ";
-            if(args.length != 0) {
-                query += "WHERE lower(COLUMN_VALUE) IN ('" + args[0].trim().toLowerCase() + "') GROUP BY isbn, title, price, COLUMN_VALUE ORDER BY COUNT(*) DESC";
-            } else {
+            for(int i = 0; i < args.length; i++){
+                if(i == 0){
+                    query += "WHERE lower(COLUMN_VALUE) IN (";
+                }
+                query += "'" + args[i].trim().toLowerCase() + "',";
+                if(i == args.length - 1){
+                    query += "'" + args[i].trim().toLowerCase() + ") GROUP BY isbn, title, price, COLUMN_VALUE ORDER BY COUNT(*) DESC"
+                }
+            }
+            if(args.length == =){
                 query += "GROUP BY isbn, title, price, COLUMN_VALUE";
             }
             ResultSet rset = stmt.executeQuery(query);
@@ -52,77 +43,11 @@ public class Search {
                 System.out.println("<td id=\"price\" scope=\"col\">" + rset.getString(4) + "</td>");
                 System.out.println("<td id=\"subject\" scope=\"col\">" + rset.getString(5) + "</td></tr>");
             }
-            /*LinkedList<Tuple> list = new LinkedList<>();
-            String query = "select book.isbn, book.title, book.price, subject.subject_name from book join subjects on book.isbn = subjects.isbn join subject on subject.subject_id = subjects.s_id ";
-            // Add all subjects to be 'OR'ed in query
-            for(int i = 0; i < args.length; i++){
-                if(i == 0){
-                    query += "where";
-                }
-                query = query + " subject.subject_name like '%" + args[i].trim().replace("-", " ") + "%' ";
-                if(i < args.length - 1){
-                    query += " or ";
-                }
-            }
-            // Get the data from search query
-            ResultSet rset = stmt.executeQuery(query);
-            while(rset.next()){
-                System.out.println(Search.isbnAlreadyInList(list, rset.getString(4), Long.parseLong(rset.getString(1))));
-                if(!Search.isbnAlreadyInList(list, rset.getString(4), Long.parseLong(rset.getString(1)))){
-                    list.add(new Tuple(1, Long.parseLong(rset.getString(1)), rset.getString(2), Double.parseDouble(rset.getString(3)), rset.getString(4)));
-                }
-            }
-            Collections.sort(list, new Comparator<Tuple>(){
-                @Override
-                public int compare(Tuple t1, Tuple t2){
-                    return t2.count - t1.count;
-                }
-            });
-            rset.close();
-            if(list.size() == 0){
-                System.out.println("<td></td><td></td><td><center><h4>No results!</h4></center></td>");
-                System.exit(0);
-            }
-            Iterator<Tuple> it = list.iterator();
-            while(it.hasNext()){
-                Tuple current = it.next();
-                System.out.println("<tr scope=\"col\">");
-                System.out.println("<td id=\"count\">" + current.count + "</td>");
-                System.out.println("<td id=\"isbn\">" + current.isbn + "</td>");
-                System.out.println("<td id=\"title\" scope=\"col\"><a href=\"cgi-bin/hyperlink.cgi?isbn=" + current.isbn + "\">" + current.title + "</a></td>");
-                System.out.println("<td id=\"price\" scope=\"col\">" + current.price + "</td>");
-                Iterator<String> ito = current.subjects.iterator();
-                System.out.println("<td id=\"subjects\" scope=\"col\">");
-                while(ito.hasNext()){
-                    String str = ito.next();
-                    System.out.println("<a href=\"cgi-bin/hyperlink.cgi?subjects=" + str.replace(" ", "-") + "\">" + str + "</a> ");
-                }
-                System.out.println("</td></tr>");
-            }*/
         }
         catch (SQLException ex) {
             System.out.println(ex);
         }
         stmt.close();
         conn.close();
-    }
-
-    // True = do not add to list (already in)
-    // False = add to list (not in list yet)
-    // Increments count if found in list
-    public static boolean isbnAlreadyInList(LinkedList<Tuple> list, String subject, long isbn){
-        // Check if 'subject' is in list
-        Iterator<Tuple> it = list.iterator();
-        while(it.hasNext()){
-            Tuple current = it.next();
-            if(current.isbn == isbn){
-                if(!current.subjects.contains(subject)){
-                    current.subjects.add(subject);
-                }
-                current.count += 1;
-                return true;
-            }
-        }
-        return false;
     }
 }
