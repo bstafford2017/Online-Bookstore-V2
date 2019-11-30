9005d7ea0f230b0f8de6977765c6e551dc6114b0
b.stafford2017 <b.stafford2017@gmail.com>
2019-11-30 17:13:33 -0600

    FIXED

--- cgi-bin/Hyperlink3.java
+++ cgi-bin/Hyperlink3.java
@@ -16,12 +16,11 @@ public class Hyperlink3 {
         try {
             Statement stmt = conn.createStatement();
             String query = "SELECT c_name, username, pwd, COLUMN_VALUE FROM customer c, table(c.purchases)(+) WHERE username LIKE '%" + args[0].trim().toLowerCase() + "%'";
-            System.out.println(query);
             ResultSet rset = stmt.executeQuery(query);
             int counter = 0;
             while(rset.next()){
                 if(counter == 0) {
-                    System.out.println("<p>Name: " + rset.getString(1) + "</p><p>Username: " + rset.getString(3) + "</p><p>Password: " + rset.getString(1) + "</p><p>Purchases: " + rset.getString(4) + " ");
+                    System.out.println("<p>Name: " + rset.getString(1) + "</p><p>Username: " + rset.getString(2) + "</p><p>Password: " + rset.getString(3) + "</p><p>Purchases: " + rset.getString(4) + " ");
                 } else {
                     System.out.println(rset.getString(4) + " ");
                 }
