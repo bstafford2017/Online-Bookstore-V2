16bf63be11cc6f9c934010fa2d2a7a8d0cd11067
b.stafford2017 <b.stafford2017@gmail.com>
2019-12-01 19:10:13 -0600

    multiple queries

--- cgi-bin/Purchase.java
+++ cgi-bin/Purchase.java
@@ -23,17 +23,11 @@ public class Purchase {
             /*
                 args[] = {username, isbn0, isbn1, ... }
             */
-            String query = "INSERT INTO table(SELECT purchases FROM customer WHERE username LIKE '" + args[0].trim() + "') VALUES (";
             for(int i = 1; i < args.length; i++){
-                if(i == args.length - 1){
-                    query += args[i].trim();
-                } else {
-                    query = query + args[i].trim() + ", ";
-                }
+                String query = "INSERT INTO table(SELECT purchases FROM customer WHERE username LIKE '" + args[0].trim() + "') VALUES (" + args[i].trim() + ")";
+                System.out.println("\n" + query);
+                stmt.executeUpdate(query);
             }
-            query += ")";
-            System.out.println("\n" + query);
-            stmt.executeUpdate(query);
         }
         catch (SQLException ex) {
             System.out.println(ex);
