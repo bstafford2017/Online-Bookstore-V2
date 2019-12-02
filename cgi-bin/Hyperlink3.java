6047078dfaeccaf12a075fa822d67631ba6723a5
b.stafford2017 <b.stafford2017@gmail.com>
2019-12-01 20:34:09 -0600

    fix

--- cgi-bin/Hyperlink3.java
+++ cgi-bin/Hyperlink3.java
@@ -23,10 +23,10 @@ public class Hyperlink3 {
                 if(counter == 0) {
                     System.out.println("<p>Name: " + rset.getString(1) + "</p><p>Username: " + rset.getString(2) + "</p><p>Password: " + rset.getString(3) + "</p><p>Purchases: ");
                 }
-                String purchaseData = "SELECT DISTINCT isbn, title, price FROM book b WHERE isbn = " + rset.getString(4);
+                String purchaseData = "SELECT isbn, title, price FROM book b WHERE isbn = " + rset.getString(4);
                 ResultSet result = innerstmt.executeQuery(purchaseData);
                 while(result.next()){
-                    System.out.println("<br/>ISBN: " + result.getString(1) + "<br/>Title: " + result.getString(2) + "<br/>Price: " + result.getString(3) + "<br/><br/>");
+                    System.out.println("<br/><br/>ISBN: " + result.getString(1) + "<br/>Title: " + result.getString(2) + "<br/>Price: " + result.getString(3) + "<br/>");
                 }
                 result.close();
                 counter++;
