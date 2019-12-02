b78024a1f60e3254df668e08dcfd64a58660fdc0
b.stafford2017 <b.stafford2017@gmail.com>
2019-12-01 21:26:55 -0600

    fix

--- listall.php
+++ listall.php
@@ -52,11 +52,9 @@
         <table class="table table-striped table-dark col-sm-6 offset-sm-3" style="margin-top: 2%;">
             <thead>
                 <tr>
-                    <th scope="col">Select</th>
                     <th scope="col">ISBN</th>
                     <th scope="col">Title</th>
                     <th scope="col">Price</th>
-                    <th scope="col">Subject(s)</th>
                 </tr>
             </thead>
             <tbody id="book-table-body">
